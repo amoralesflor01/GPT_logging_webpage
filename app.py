ba2cefe82d6081957214e4c2cea135181bf41234
tgirma11 <girmalegesseterfa@gmail.com>
2024-10-06 02:23:29 -0700

    Added the chat and email on same page(not done yet )

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ api_key = my_api_key
 client = OpenAI(api_key=api_key)
 
 # Set the default timer in seconds
-TIMER_LIMIT = 300
+TIMER_LIMIT = 900
 
 # Define the name of the bot
 name = 'Assitant'
