54aeb0f2a287be7b369ccbe93c4760dfa6ffec83
Alexis Morales Flores <amoralesflor01@proton.me>
2024-12-10 15:33:56 -0800

    Added new instructions for control group.

--- services/create_accounts_in_db.py
+++ services/create_accounts_in_db.py
@@ -2,7 +2,10 @@ import services.data_service as svc
 
 # user_ids = ['12345', '67890', '11122', '33344', '55678', '99001', '23456', '78901', '34567', '89012', '56789', '10123', '67812', '91234', '0', '1']
 
-user_ids = ['0', '1', '2', '3']
+# For Testing the functionality of the displaying of same email to 1 user from each group.
+# user_ids = ['0', '1', '2', '3']
+
+user_ids = ['24080', '12442', '23492', '66612', '00800', '19237', '95123', '81739', '46791', '68315']
 
 # function generate_users(n (n is even)):
 # step 1: pick random uuid for email (calls pickRandomEmailUuid())
